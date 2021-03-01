a25ddb97747c9ca584f038736a0a9e6c95aff620
alkhateeb49 <mohamadalkhateeb49@yahoo.com>
2021-03-01 21:19:35 +0200

    lab12

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(express.urlencoded({ extended: true }));
 //
 //
 let pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL);
-// const client = new pg.Client({ connectionString: process.env.DATABASE_URL,ssl: { rejectUnauthorized: false } });
+// const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL,ssl: { rejectUnauthorized: false } });
 //
 
 app.get('/', favRender);
